2ae727607136c3828add3dec774a215daa4db0de
AYUSH DUMASIA <ayushdumasia00@gmail.com>
2024-02-12 12:36:23 +0530

    customize cart feature

--- routes/listing.js
+++ routes/listing.js
@@ -16,40 +16,44 @@ router.get("/new", (req, res) => {
 //For Cart Route
 router.get("/cart", async (req, res) => {
     const nonSavedListings = await Listing.find({ saved: true });
+    let counts = await Listing.countDocuments({saved : true});
     if(nonSavedListings.length === 0){
-        res.render("listing/empty.ejs");
+        res.render("listing/empty.ejs" , {counts});
     }
     else{
         const allListings = await Listing.find({ saved: true });
-        res.render("listing/cart.ejs", { allListings });
+        res.render("listing/cart.ejs", { allListings , counts });
     }
 })
 
 //Main Route
 router.get("/", async (req, res) => {
+    let counts = await Listing.countDocuments({saved : true});
     let allListings = await Listing.find({});
-    res.cookie("name " , "cokkie");
-    res.render("listing/index.ejs", { allListings: allListings });
-    console.dir(req.cookies);
+    console.log(counts);
+    res.render("listing/index.ejs", { allListings: allListings , counts : counts });
 })
 
 
 //For Ascending Route
 router.get("/asc", async (req, res) => {
+    let counts = await Listing.countDocuments({saved : true});
     const allListings = await Listing.find().sort({ price: 1 });
-    res.render("listing/index.ejs", { allListings });
+    res.render("listing/index.ejs", { allListings , counts  });
 });
 
 //For descending Route
 router.get("/desc", async (req, res) => {
+    let counts = await Listing.countDocuments({saved : true});
     const allListings = await Listing.find().sort({ price: -1 });
-    res.render("listing/index.ejs", { allListings });
+    res.render("listing/index.ejs", { allListings , counts });
 });
 
 
 router.patch("/:id", async (req, res) => {
     let { id } = req.params;
     await Listing.findByIdAndUpdate({ _id: id }, { $set: { saved: true } });
+    let counts = await Listing.countDocuments({saved : true});
     // alert("saver");
     res.redirect("/listing");
 })
@@ -57,14 +61,23 @@ router.patch("/:id", async (req, res) => {
 //Remove From Cart
 router.patch("/cart/:id", async (req, res) => {
     let { id } = req.params;
+    let counts = await Listing.countDocuments({saved : true});
+    const nonSavedListings = await Listing.find({ saved: true });
     let result = await Listing.findByIdAndUpdate({ _id: id }, { $set: { saved: false } });
-    res.redirect("/listing/cart");
+    if(nonSavedListings.length === 0){
+        res.render("listing/empty.ejs" , {counts});
+    }
+    else{
+        const allListings = await Listing.find({ saved: true });
+        res.render("listing/cart.ejs", { allListings , counts });
+    }
     // console.log(result);
 })
 
 
 //Search Route
 router.get("/search", async (req, res) => {
+    let counts = await Listing.countDocuments({saved : true});
     let match = {};
         if (req.query.keyword) {
             match.title = new RegExp(req.query.keyword, "i");
@@ -88,16 +101,17 @@ router.get("/search", async (req, res) => {
         ]).exec();
         console.log(allListings);
         if(allListings.length === 0){
-            res.render("listing/emptySearch.ejs");
+            res.render("listing/emptySearch.ejs" , {counts : counts});
         }
         else{
-            res.render("listing/index.ejs", { allListings: allListings });
+            res.render("listing/index.ejs", { allListings: allListings , counts : counts });
         }
 
 })
 
 
 router.post("/", async (req ,res , next) =>{
+    let counts = await Listing.countDocuments({saved : true});
     let result = listingSchema.validate(req.body);
     console.log(result);
     let {username, title, price, image, description, location , country } = req.body;
@@ -121,22 +135,25 @@ router.post("/", async (req ,res , next) =>{
 
 //Show Post Route
 router.get("/:id", async (req, res) => {
+    let counts = await Listing.countDocuments({saved : true});
     let { id } = req.params;
     const listingData = await Listing.findById(id).populate("reviews");
     // res.send("working");
-    res.render("listing/show.ejs", { listingData });
+    res.render("listing/show.ejs", { listingData , counts});
 })
 
 
 5
 //Edit Route
 router.get("/:id/edit", async (req, res) => {
+    let counts = await Listing.countDocuments({saved : true});
     let { id } = req.params;
     const UpdateData = await Listing.findById(id);
-    res.render("edit.ejs", { UpdateData });
+    res.render("edit.ejs", { UpdateData , counts });
 })
 
 router.put("/:id", async (req, res) => {
+    let counts = await Listing.countDocuments({saved : true});
     let { id } = req.params;
     let { title: title, price: price,image : image, description: description, location: location , country : country} = req.body;
     let updateCard = await Listing.findByIdAndUpdate(
@@ -164,6 +181,7 @@ router.put("/:id", async (req, res) => {
 
 //Delete Route
 router.delete("/:id", async (req, res) => {
+    let counts = await Listing.countDocuments({saved : true});
     let { id } = req.params;
     await Listing.findByIdAndDelete(id);
     res.redirect("/listing");
